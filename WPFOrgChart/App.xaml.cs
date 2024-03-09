0412bf4792c94a70eb3f7db82ed762ac53c21763
samikshaaghodke <samikshaaghodke@gmail.com>
2024-03-09 10:49:52 +0100

    Add project files.

---
+++ WPFOrgChart/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace OrgHierarchy
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application {
+        private void Application_Startup(object sender, StartupEventArgs e) {
+            Initialize();
+            //Finally call the window.
+            MainWindow mw = new MainWindow();
+            mw.ShowDialog();
+        }
+
+        void Initialize()
+        {
+            //to initialize services or classes or objects as needed.
+
+        }
+    }
+}
---
+++ WPFOrgChart/Enums/TargetType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrgHierarchy.Enums {
+    public enum TargetType {
+        Employee,
+        Role,
+        Department
+    }
+}
